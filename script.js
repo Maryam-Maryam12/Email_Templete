27b906ce687315d8640eaa83a5c42a028177dc7d
Maryam-Maryam12 <maryamshaukat890@gmail.com>
2024-09-25 16:58:03 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ document.querySelector('.cta-button').addEventListener('mouseover', function () {
+            this.style.transform = 'scale(1.05)';
+        });
+
+        document.querySelector('.cta-button').addEventListener('mouseout', function () {
+            this.style.transform = 'scale(1)';
+        });
